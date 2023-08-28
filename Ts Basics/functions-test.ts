b9449ada539a2b04bed6bb1d1fa7ac1965244de2
Shpeta <shpetimbeqiri08@gmail.com>
2023-08-28 13:16:13 +0200

    challenge 2 beginning

---
+++ Ts Basics/functions-test.ts
@@ -0,0 +1,5 @@
+import addNumbers, {addStrings, getName} from "./functions";
+
+console.log(addNumbers(1, 2));
+console.log(addStrings('a'));
+console.log(getName({first: "Jack", last: "herrington"}));
